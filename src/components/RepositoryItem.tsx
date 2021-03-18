69e591fc9c658bb4b102da098b933b5001341b42
Emerson Freitas <emersonfreitasn@gmail.com>
2021-03-18 11:24:22 -0500

    componentes com typescript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,4 +1,12 @@
-export function RepositoryItem(props) {
+type RespositoryItemProps = {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+};
+
+export function RepositoryItem(props: RespositoryItemProps) {
   return (
     <li>
       <strong>{props.repository.name}</strong>
